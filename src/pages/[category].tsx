532dea51f08cdfbfbcaf8148f2845b34c7f43b16
samuel ho <samuelho343@gmail.com>
2023-04-05 10:00:13 -0700

    category pages

---
+++ src/pages/[category].tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function categorypage({ category }) {
+  return <div>categorypage</div>;
+}
+
+export default categorypage;
---
+++ src/pages/[category]/[subcategory].tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function subcategorypage({ subcategory }) {
+  return <div>subcategorypage</div>;
+}
+
+export default subcategorypage;
