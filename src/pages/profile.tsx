c036325de851cc5488089a5852a63594736357aa
samuel ho <samuelho343@gmail.com>
2023-04-05 10:05:54 -0700

    init

---
+++ src/pages/profile.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function profile() {
+  return (
+    <div className="flex h-full w-full max-w-3xl lg:max-w-5xl">
+      <div className="h-full w-2/5">
+        <div>
+          <h3>Instruments ⬇️</h3>
+          <div>List</div>
+        </div>
+      </div>
+      <div className="w-3/5">Main</div>
+    </div>
+  );
+}
+
+export default profile;
