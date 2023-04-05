6802c1f0987b0eca56e668686b73132804b442ba
samuel ho <samuelho343@gmail.com>
2023-04-05 10:11:19 -0700

    quickfix

--- src/env.mjs
+++ src/env.mjs
@@ -20,10 +20,7 @@ const server = z.object({
   ),
   // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
 
-  EMAIL_SERVER_USER: z.string(),
-  EMAIL_SERVER_PASSWORD: z.string(),
-  EMAIL_SERVER_HOST: z.string(),
-  EMAIL_SERVER_PORT: z.string(),
+  EMAIL_SERVER: z.string(),
   EMAIL_FROM: z.string(),
 });
 
@@ -46,10 +43,7 @@ const processEnv = {
   NODE_ENV: process.env.NODE_ENV,
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  EMAIL_SERVER_USER: process.env.EMAIL_SERVER_USER,
-  EMAIL_SERVER_PASSWORD: process.env.EMAIL_SERVER_PASSWORD,
-  EMAIL_SERVER_HOST: process.env.EMAIL_SERVER_HOST,
-  EMAIL_SERVER_PORT: process.env.EMAIL_SERVER_PORT,
+  EMAIL_SERVER: process.env.EMAIL_SERVER,
   EMAIL_FROM: process.env.EMAIL_FROM,
   // process.env.NEXTAUTH_URLnv.NEXT_PUBLIC_CLIENTVAR,
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,10 +8,9 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+const MyApp: AppType<{
+  session: Session | null;
+}> = ({ Component, pageProps: { session, ...pageProps } }) => {
   return (
     <SessionProvider session={session}>
       <Head>
