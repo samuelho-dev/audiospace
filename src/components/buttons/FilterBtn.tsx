0d319419b66370d42b672750d084a77a28905662
samuel ho <samuelho343@gmail.com>
2023-04-10 12:44:44 -0700

    filter buttons

---
+++ src/components/buttons/FilterBtn.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+function FilterBtn({ children }) {
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
+  return (
+    <div
+      onClick={handleClick}
+      className={`cursor-pointer rounded-lg px-2 py-1 text-xs outline outline-1 outline-zinc-400  ${
+        clicked ? "bg-zinc-600 hover:bg-zinc-500" : "bg-none hover:bg-zinc-700"
+      }`}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default FilterBtn;
