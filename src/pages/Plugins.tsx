fd9c573fabdef4ec9171eab220c35c7bc56afada
samuel ho <samuelho343@gmail.com>
2023-04-09 20:24:50 -0700

    dynamic routing for filter

--- src/pages/Plugins.tsx
+++ src/pages/Plugins.tsx
@@ -1,50 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { api } from "~/utils/api";
 import Main from "~/components/plugin/main";
 import { useRouter } from "next/router";
 import { type CategorySchema } from "~/types/schema";
 import Sidebar from "~/components/plugin/Sidebar";
 import { BiSearchAlt } from "react-icons/bi";
+import FilterBtn from "~/components/buttons/FilterBtn";
+import { useRouterFilter } from "~/utils/useRouterFilter";
 
 interface FilterProps {
   categories: CategorySchema[];
+  handleRoute: Function;
 }
 
 interface ProfileRouteProps {
   route?: string;
 }
 
-function getRoute(section: string | string[] | undefined): string {
-  return typeof section === "string" ? section : "";
-}
+function Filters({ categories, handleRoute }: FilterProps) {
+  const handleCategoryClick = (categoryName: string) => {
+    handleRoute(categoryName);
+  };
 
-function PluginRoute({ route }: ProfileRouteProps) {
-  console.log(route);
-  switch (route) {
-    case "":
-      return <Main />;
-    case "effect":
-      return <Main />;
-    case "instrument":
-      return <Main />;
-    default:
-      return <Main />;
-  }
-}
-
-function Filters({ categories }: FilterProps) {
+  const handleSubcategoryClick = (
+    categoryName: string,
+    subcategoryName: string
+  ) => {
+    handleRoute(categoryName, subcategoryName);
+  };
   return (
     <div className="flex w-full flex-col justify-center gap-4 rounded-lg p-4 outline outline-1 outline-zinc-700">
       <div className="flex gap-2">
         <h5>Types :</h5>
         <div className="flex gap-3">
           {categories.map((category) => (
-            <p
-              key={category.id}
-              className="cursor-pointer rounded-lg px-2 py-1 text-xs outline outline-1 outline-zinc-400 hover:bg-zinc-700"
-            >
-              {category.name}
-            </p>
+            <FilterBtn key={category.id}>
+              <p onClick={() => handleCategoryClick(category.name)}>
+                {category.name}
+              </p>
+            </FilterBtn>
           ))}
         </div>
       </div>
@@ -53,16 +47,20 @@ function Filters({ categories }: FilterProps) {
         <div className="flex flex-wrap gap-3">
           {categories.map((category) => (
             <>
-              {category.subcategories.map((subcategories) => (
-                <div
-                  key={subcategories.id}
-                  className="flex w-fit cursor-pointer items-center justify-center gap-4 rounded-lg px-2 py-1 pl-2 outline outline-1 outline-zinc-400 hover:bg-zinc-700"
-                >
-                  <p className="text-xs">{subcategories.name}</p>
-                  <p className="text-xs text-zinc-400">
-                    {subcategories._count.products}
-                  </p>
-                </div>
+              {category.subcategories.map((subcategory) => (
+                <FilterBtn key={subcategory.id}>
+                  <div
+                    className="flex gap-2"
+                    onClick={() =>
+                      handleSubcategoryClick(category.name, subcategory.name)
+                    }
+                  >
+                    <p className="text-xs">{subcategory.name}</p>
+                    <p className="text-xs text-zinc-400">
+                      {subcategory._count.products}
+                    </p>
+                  </div>
+                </FilterBtn>
               ))}
             </>
           ))}
@@ -71,18 +69,27 @@ function Filters({ categories }: FilterProps) {
     </div>
   );
 }
+function FilterProductView() {
+  return <div>Filter</div>;
+}
 
 function Plugins() {
   const router = useRouter();
-  const { filter } = router.query;
+  const { category, tag } = router.query;
+  const { handleRoute } = useRouterFilter();
+  console.log(category, tag);
   const categoriesQuery = api.onload.getCategories.useQuery();
-  const productsQuery = api.products.getProducts.useQuery({
-    text: "Distortion",
+  const productsQuery = api.products.getFilteredProducts.useQuery({
+    categories: Array.isArray(category) ? category : [],
+    subcategories: Array.isArray(tag) ? tag : [],
   });
   console.log(productsQuery);
+
   return (
     <div className="flex min-h-screen w-full max-w-3xl gap-8 lg:max-w-5xl">
-      {categoriesQuery.data && <Sidebar categories={categoriesQuery.data} />}
+      {categoriesQuery.data && (
+        <Sidebar categories={categoriesQuery.data} handleRoute={handleRoute} />
+      )}
       <div className="flex w-full flex-col gap-4 pt-12">
         <div className="flex w-full justify-center gap-2">
           <input
@@ -93,7 +100,13 @@ function Plugins() {
             <BiSearchAlt size={25} />
           </div>
         </div>
-        {categoriesQuery.data && <Filters categories={categoriesQuery.data} />}
+        {categoriesQuery.data && (
+          <Filters
+            categories={categoriesQuery.data}
+            handleRoute={handleRoute}
+          />
+        )}
+        {category || tag ? <FilterProductView /> : <Main />}
 
         {/* <PluginRoute route={getRoute(section)} /> */}
       </div>
--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -2,6 +2,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
 import { onloadRouter } from "~/server/api/routers/onload";
 import { mailRouter } from "~/server/api/routers/mail";
 import { userProfileRouter } from "./routers/userprofile";
+import { productRouter } from "./routers/products";
 
 /**
  * This is the primary router for your server.
@@ -9,6 +10,7 @@ import { userProfileRouter } from "./routers/userprofile";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
+  products: productRouter,
   userprofile: userProfileRouter,
   onload: onloadRouter,
   mail: mailRouter,
