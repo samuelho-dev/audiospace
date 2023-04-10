f9165879aeaff9a8c0f06af956bb0547bbcb53f3
samuel ho <samuelho343@gmail.com>
2023-04-09 20:26:52 -0700

    filter api

---
+++ src/server/api/routers/plugins.ts
@@ -0,0 +1,44 @@
+import { z } from "zod";
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+} from "~/server/api/trpc";
+import { CategorySchema, ProductSchema } from "~/types/schema";
+
+export const onloadRouter = createTRPCRouter({
+  getPopularEffect: publicProcedure
+    // .output(z.array(ProductSchema))
+    .query(async ({ ctx }) => {
+      const data = await ctx.prisma.product.findMany({
+        select: {
+          category: {
+            name: "Effect",
+          },
+        },
+        take: 10,
+        orderBy: {
+          transactions: {
+            _count: "desc",
+          },
+        },
+      });
+
+      return data;
+    }),
+
+  getPopularInstruments: publicProcedure
+    // .output(z.array(ProductSchema))
+    .query(async ({ ctx }) => {
+      const data = await ctx.prisma.product.findMany({
+        take: 10,
+        orderBy: {
+          transactions: {
+            _count: "desc",
+          },
+        },
+      });
+
+      return data;
+    }),
+});
