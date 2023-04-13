21188a7d7df81161149ea54a3183c51a18e7e671
samuel ho <samuelho343@gmail.com>
2023-04-13 15:34:55 -0700

    start battle form init

--- src/pages/battles/startbattle.tsx
+++ src/pages/battles/startbattle.tsx
@@ -12,7 +12,27 @@ function StartBattle() {
   if (status === "authenticated") {
     return (
       <form>
-        <h2>Start a</h2>
+        <h2>Start a battle</h2>
+        <div className="flex flex-col">
+          <label>
+            Title: <input type="text" />
+          </label>
+          <div>
+            <label>Description:</label>
+            <input type="text" />
+          </div>
+          <label className="mb-2 block text-sm font-medium text-white">
+            Upload Profile Picture
+          </label>
+          <input
+            id="profileImg"
+            name="profileImg"
+            type="file"
+            multiple={true}
+            accept="mp3/*, wav/*"
+            className="block w-full cursor-pointer rounded-lg border border-gray-600 bg-gray-700 text-sm text-gray-400 placeholder-gray-400 focus:outline-none"
+          />
+        </div>
       </form>
     );
   }
