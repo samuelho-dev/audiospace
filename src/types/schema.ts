48f803b35315a9adbcad23fc96f711ac07d67f51
samuelho-dev <samuelho343@gmail.com>
2023-04-23 20:58:51 -0700

    add cart schema

--- src/types/schema.ts
+++ src/types/schema.ts
@@ -1,8 +1,12 @@
 import { z } from "zod";
 
-export const SellerSchema = z.object({
+export const ProductImageSchema = z.object({
   id: z.number(),
-  name: z.string(),
+  imageUrl: z.string(),
+});
+
+export const SellerSchema = z.object({
+  user: z.object({ id: z.number().optional(), username: z.string() }),
 });
 
 export const SubcategorySchema = z.object({
@@ -26,14 +30,15 @@ export const CategorySchema = z.object({
 export const ProductSchema = z.object({
   id: z.number(),
   seller: SellerSchema,
+  description: z.string().optional(),
   name: z.string(),
   images: z.array(z.unknown()),
-  category: CategorySchema,
-  subcategory: z.array(SubcategorySchema),
   price: z.number(),
   preview_url: z.string().nullable(),
   discount_rate: z.number(),
-  wishlist_users: z.array(z.unknown()),
+  category: CategorySchema,
+  subcategory: z.array(SubcategorySchema),
+  wishlist_users: z.array(z.unknown()).optional(),
 });
 
 export const UserSchema = z.object({
