0ed6df526cb335873e005071cb4a1c43084acdc7
samuelho-dev <samuelho343@gmail.com>
2023-04-23 21:11:48 -0700

    product card api calls

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -3,25 +3,49 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { BsHeart, BsHeartHalf, BsHeartFill } from "react-icons/bs";
-import { VscAdd } from "react-icons/vsc";
+import { HiOutlineShoppingCart, HiShoppingCart } from "react-icons/hi";
 import { type ProductSchema } from "~/types/schema";
 import { RiPlayMiniLine } from "react-icons/ri";
 import { UseRouterFilter } from "~/utils/useRouterFilter";
+import { api } from "~/utils/api";
 
 interface ProductCardProps {
   product: ProductSchema;
 }
 
 function ProductCard({ product }: ProductCardProps) {
+  const { data: session } = useSession();
   const { asPath, query } = useRouter();
   const [previewHover, setPreviewHover] = useState(false);
+  const [favorite, setFavorite] = useState(false);
+  const [cartItem, setCartItem] = useState(false);
   const { handleRoute } = UseRouterFilter();
   const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
-
   // useEffect(() => (setAudio(new Audio(product.preview_url));), [product.preview_url]);
+
+  const userWishlistQuery = api.userprofile.getWishlist.useQuery();
+  const wishlistAddMutation =
+    api.userprofile.addProductToWishlist.useMutation();
+  const wishlistRemoveMutation =
+    api.userprofile.deleteProductFromWishlist.useMutation();
+  const handleWishlistAdd = () => {
+    setFavorite(true);
+    void wishlistAddMutation.mutateAsync({
+      id: product.id,
+    });
+  };
+
+  console.log(product);
+  const handleWishlistRemove = () => {
+    setFavorite(false);
+    void wishlistRemoveMutation.mutateAsync({
+      id: product.id,
+    });
+  };
 
   return (
     <div className="cardShadow my-4 flex h-60 w-40 flex-col justify-between rounded-b-lg rounded-r-lg bg-zinc-900 p-4 hover:bg-zinc-800">
+      {" "}
       <div
         className="relative h-28 w-32 justify-center"
         onMouseOver={() => setPreviewHover(true)}
@@ -41,12 +65,11 @@ function ProductCard({ product }: ProductCardProps) {
           loading="lazy"
         />
       </div>
-
       <h5 className="scrollbar-hide overflow-x-scroll whitespace-nowrap py-2 hover:cursor-pointer">
         {product?.name}
       </h5>
       <p className="text-sm text-gray-300 hover:cursor-pointer hover:underline hover:underline-offset-2">
-        {product?.seller.name}
+        {product?.seller.user.username}
       </p>
       <div className="scrollbar-hide flex gap-1 overflow-scroll whitespace-nowrap">
         {product?.subcategory.map((subcategory, i) => (
@@ -60,16 +83,34 @@ function ProductCard({ product }: ProductCardProps) {
           </p>
         ))}
       </div>
-
       <div className="flex justify-between pt-2">
         <h5 className="text-gray-200">${product?.price}</h5>
-        <div className="flex items-center gap-4">
-          <h5>
-            <BsHeart className="cursor-pointer hover:fill-zinc-500" />
-          </h5>
-          <h5>
-            <VscAdd className="cursor-pointer hover:fill-zinc-500" />
-          </h5>
+        <div className="flex items-center gap-2">
+          {(userWishlistQuery.data &&
+            userWishlistQuery.data.includes(product.id)) ||
+          favorite ? (
+            <BsHeartFill
+              onClick={handleWishlistRemove}
+              className="cursor-pointer hover:fill-zinc-500"
+            />
+          ) : (
+            <BsHeart
+              onClick={handleWishlistAdd}
+              className="cursor-pointer hover:fill-zinc-500"
+            />
+          )}
+
+          {cartItem ? (
+            <HiShoppingCart
+              size={20}
+              className="cursor-pointer hover:fill-zinc-500"
+            />
+          ) : (
+            <HiOutlineShoppingCart
+              size={20}
+              className="cursor-pointer hover:fill-zinc-500"
+            />
+          )}
         </div>
       </div>
     </div>
