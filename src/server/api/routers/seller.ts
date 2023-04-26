cba96f20c3d366d78be0a81b5bc6725a50a0fad2
samuelho-dev <samuelho343@gmail.com>
2023-04-25 22:46:23 -0700

    seller api

---
+++ src/server/api/routers/seller.ts
@@ -0,0 +1,89 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+} from "~/server/api/trpc";
+import { ProductSchema } from "~/types/schema";
+import cloudinary from "~/utils/cloudinary";
+import uploadCloudinary from "~/utils/uploadCloudinary";
+
+const sessionSchema = z.object({
+  user: z.object({
+    image: z.string(),
+    email: z.string().email(),
+  }),
+});
+
+export const sellerProfileRouter = createTRPCRouter({
+  getSellerProduct: protectedProcedure
+    .output(
+      z.object({
+        id: z.number(),
+        userId: z.string(),
+        products: z.array(ProductSchema),
+      })
+    )
+    .query(({ ctx }) => {
+      const data = ctx.prisma.seller.findFirstOrThrow({
+        where: {
+          userId: ctx.session.user.id,
+        },
+        include: {
+          products: {
+            select: {
+              id: true,
+              seller: {
+                include: {
+                  user: {
+                    select: {
+                      username: true,
+                    },
+                  },
+                },
+              },
+              description: true,
+              category: true,
+              subcategory: true,
+              name: true,
+              images: true,
+              price: true,
+              discount_rate: true,
+            },
+          },
+        },
+      });
+
+      return data;
+    }),
+  updateSellerProduct: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        name: z.string().min(1),
+        description: z.string().min(5),
+        price: z.number(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const seller = await ctx.prisma.seller.findUniqueOrThrow({
+        where: {
+          userId: ctx.session.user.id,
+        },
+      });
+
+      const data = await ctx.prisma.product.update({
+        where: {
+          sellerId: seller.id,
+          id: input.id,
+        },
+        data: {
+          name: input.name,
+          description: input.description,
+          price: input.price,
+        },
+      });
+      return data;
+    }),
+});
