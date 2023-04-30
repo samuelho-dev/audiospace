45b805d1f4ba88f82ecd17e8b0e5d3bd45dc4170
samuelho-dev <samuelho343@gmail.com>
2023-04-29 20:42:34 -0700

    submission page, removed beat component

--- src/components/profile/submissions.tsx
+++ src/components/profile/submissions.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { api } from "~/utils/api";
+import soundCloudUrl from "~/utils/soundcloudUrl";
 
 function Submissions() {
-  return <div>submissions</div>;
+  const submissionQuery = api.userprofile.getPastBeatSubmissions.useQuery();
+
+  console.log(submissionQuery.data);
+  return (
+    <div>
+      <h3>Submissions</h3>
+      <div className="h-40">
+        {submissionQuery.data &&
+          submissionQuery.data.map((submission) => (
+            <div key={submission.id} className="flex justify-between">
+              {/* <p>{submission.subimittedAt.toLocaleString()}</p> */}
+              <iframe
+                allow="autoplay"
+                src={soundCloudUrl(submission.trackUrl)}
+                height="20"
+              ></iframe>
+              <p>Rating : {submission.rating}</p>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 }
 
 export default Submissions;
