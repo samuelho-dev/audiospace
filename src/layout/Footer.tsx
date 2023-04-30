2e52fb69ab3fde2a21701f9bae439f3fe8a6246b
samuelho-dev <samuelho343@gmail.com>
2023-04-29 20:42:42 -0700

    footer start outline

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -3,9 +3,10 @@ import React from "react";
 function Footer() {
   return (
     <div className="flex w-full max-w-3xl justify-between lg:max-w-5xl">
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
+      <p className="font-xs text-sm">ABOUT</p>
+      <p className="font-xs text-sm">HELP & FAQ</p>
+      <p className="font-xs text-sm">CONTACT</p>
+      <p className="font-xs text-md">audiospace @ 2023</p>
     </div>
   );
 }
