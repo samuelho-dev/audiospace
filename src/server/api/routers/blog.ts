80b3a0b4283394755aa60a2001aee9a6b108e159
samuelho-dev <samuelho343@gmail.com>
2023-05-02 12:13:24 -0700

    blog api calls

---
+++ src/server/api/routers/blog.ts
@@ -0,0 +1,69 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+} from "~/server/api/trpc";
+import { encode } from "~/utils/quickHash";
+
+import uploadB2 from "~/utils/uploadB2";
+
+export const blogRouter = createTRPCRouter({
+  getPost: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const data = await ctx.prisma.post.findFirstOrThrow({
+        where: {
+          id: input.id,
+        },
+      });
+      return data;
+    }),
+  getBlogPosts: publicProcedure.query(async ({ ctx }) => {
+    const data = await ctx.prisma.post.findMany({
+      where: {},
+      include: {
+        tag: true,
+      },
+      take: 20,
+    });
+
+    return data;
+  }),
+  getBlogTags: publicProcedure.query(async ({ ctx }) => {
+    const data = ctx.prisma.tag.findMany({});
+    return data;
+  }),
+  uploadBlogPosts: protectedProcedure
+    .input(
+      z.object({
+        title: z.string(),
+        description: z.string(),
+        file: z.string(),
+        blogTag: z.number(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const uploadProduct = await uploadB2(input.file, "AudiospaceBlog");
+
+      if (!uploadProduct) {
+        throw new Error("Error uploading post");
+      }
+      const data = await ctx.prisma.post.create({
+        data: {
+          id: encode(input.title.replace(" ", "-").toLowerCase()),
+          title: input.title,
+          description: input.description,
+          contentUrl: uploadProduct,
+          tag: {
+            connect: {
+              id: input.blogTag,
+            },
+          },
+        },
+      });
+
+      return data;
+    }),
+});
