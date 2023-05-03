7f6697889cb5111f171571124b2176394196e214
samuelho-dev <samuelho343@gmail.com>
2023-05-03 16:42:35 -0700

    add mdx and tailwind mdx prose

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,22 @@
+import nextMDX from "@next/mdx";
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
 !process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
+
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    // If you use remark-gfm, you'll need to use next.config.mjs
+    // as the package is ESM only
+    // https://github.com/remarkjs/remark-gfm#install
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+});
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -29,4 +43,5 @@ const config = {
     ],
   },
 };
-export default config;
+
+export default withMDX(config);
