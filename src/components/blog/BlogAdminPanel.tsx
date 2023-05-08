da895348bd7fadb2846de868300b598505e08dc9
samuelho-dev <samuelho343@gmail.com>
2023-05-07 17:49:33 -0700

    added image upload - blog

--- src/components/blog/BlogAdminPanel.tsx
+++ src/components/blog/BlogAdminPanel.tsx
@@ -1,42 +1,47 @@
 import React, { useState } from "react";
 import { api } from "~/utils/api";
 import { StandardB2Dropzone } from "../dropzone/StandardB2Dropzone";
-import { stringify } from "superjson";
+import { readFileasBase64 } from "~/utils/readFileAsBase64";
 
 function BlogAdminPanel() {
   const [newPost, setNewPost] = useState({
     title: "",
     description: "",
     blogTag: 1,
-    image: "",
+    imageUrl: "",
     file: "",
   });
-
   const [uploadedFile, setUploadedFile] = useState(false);
+  const [uploadedImage, setUploadedImage] = useState(false);
   const blogPostMutation = api.blog.uploadBlogPosts.useMutation();
   const blogTagsQuery = api.blog.getBlogTags.useQuery();
-  const bannerQuery = api.cloudinary.uploadImages.useMutation();
+  const uploadCloudinaryMutation = api.cloudinary.uploadImages.useMutation();
 
   const handleNewBlogPost = async () => {
     const post = { ...newPost };
     post.blogTag = Number(post.blogTag);
-
-    const data = await bannerQuery.mutateAsync({
-      images: [newPost.image],
-      folder: "user",
-    });
-    post.image = data[0]?.imageUrl as string;
-
     await blogPostMutation.mutateAsync(post);
   };
 
-  const bannerUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const blogImageState = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const { files } = e.target;
     if (files) {
       const file = files[0] as File;
-      const data = stringify(file);
-      setNewPost((prevData) => ({ ...prevData, image: data }));
+      const data = await readFileasBase64(file);
+      setNewPost((prevData) => ({ ...prevData, imageUrl: data }));
     }
+  };
+
+  const blogImageUpload = async () => {
+    const data = await uploadCloudinaryMutation.mutateAsync({
+      folder: "blog",
+      images: [newPost.imageUrl],
+    });
+    setNewPost((prevData) => ({
+      ...prevData,
+      imageUrl: data[0]?.imageUrl as string,
+    }));
+    setUploadedImage(true);
   };
 
   const handleFileChange = (value: string) => {
@@ -51,58 +56,73 @@ function BlogAdminPanel() {
   };
 
   return (
-    <div>
-      <div>
-        <div className="flex w-full flex-col">
-          <label>Title</label>
-          <input
-            className="text-black"
-            name="title"
-            type="text"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex w-full flex-col">
-          <label>Description</label>
-          <input
-            className="text-black"
-            type="text"
-            name="description"
-            onChange={handleChange}
-          />
-        </div>
-        <input type="file" className="py-2" onChange={bannerUpload} />
-        {uploadedFile ? (
-          <h3>Submitted</h3>
-        ) : (
-          <StandardB2Dropzone
-            bucket="AudiospaceBlog"
-            handleFileChange={handleFileChange}
-            setUploadedFile={setUploadedFile}
-          />
-        )}
-        <div className="flex flex-col gap-2">
-          <label>Blog Type</label>
-          <select
-            name="blogTag"
-            className="w-fit text-black"
-            onChange={handleChange}
-          >
-            {blogTagsQuery.data &&
-              blogTagsQuery.data.map((tag) => (
-                <option key={tag.id} value={tag.id}>
-                  {tag.name}
-                </option>
-              ))}
-          </select>
-        </div>
-        <button
-          className="my-2 border border-zinc-300 px-2"
-          onClick={() => void handleNewBlogPost()}
-        >
-          SUBMIT
-        </button>
+    <div className="rounded-lg bg-zinc-900 p-4">
+      <div className="flex w-full flex-col">
+        <label>Title</label>
+        <input
+          className="text-black"
+          name="title"
+          type="text"
+          onChange={handleChange}
+        />
       </div>
+      <div className="flex w-full flex-col">
+        <label>Description</label>
+        <input
+          className="text-black"
+          type="text"
+          name="description"
+          onChange={handleChange}
+        />
+      </div>
+      {uploadedImage ? (
+        <h3>Submitted</h3>
+      ) : (
+        <div className="my-2 flex">
+          <input
+            type="file"
+            accept="/png, /jpeg,"
+            multiple={false}
+            onChange={(e) => void blogImageState(e)}
+          />
+          <button
+            className="border border-zinc-200 px-2"
+            onClick={() => void blogImageUpload()}
+          >
+            Submit Image
+          </button>
+        </div>
+      )}
+      {uploadedFile ? (
+        <h3>Submitted</h3>
+      ) : (
+        <StandardB2Dropzone
+          bucket="AudiospaceBlog"
+          handleFileChange={handleFileChange}
+          setUploadedFile={setUploadedFile}
+        />
+      )}
+      <div className="flex flex-col">
+        <label>Blog Type</label>
+        <select
+          name="blogTag"
+          className="w-fit text-black"
+          onChange={handleChange}
+        >
+          {blogTagsQuery.data &&
+            blogTagsQuery.data.map((tag) => (
+              <option key={tag.id} value={tag.id}>
+                {tag.name}
+              </option>
+            ))}
+        </select>
+      </div>
+      <button
+        className="my-2 border border-zinc-300 bg-zinc-600 px-2"
+        onClick={() => void handleNewBlogPost()}
+      >
+        SUBMIT
+      </button>
     </div>
   );
 }
