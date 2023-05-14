04bbaf23b411e500502d98db27392ed875007de4
samuelho-dev <samuelho343@gmail.com>
2023-05-14 13:02:27 -0700

    remove mdx

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,8 @@
-import nextMDX from "@next/mdx";
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
 !process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
-
-const withMDX = nextMDX({
-  extension: /\.mdx?$/,
-  options: {
-    // If you use remark-gfm, you'll need to use next.config.mjs
-    // as the package is ESM only
-    // https://github.com/remarkjs/remark-gfm#install
-    remarkPlugins: [],
-    rehypePlugins: [],
-    // If you use `MDXProvider`, uncomment the following line.
-    // providerImportSource: "@mdx-js/react",
-  },
-});
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -44,4 +30,4 @@ const config = {
   },
 };
 
-export default withMDX(config);
+export default config;
