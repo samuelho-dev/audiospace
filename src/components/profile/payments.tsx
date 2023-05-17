52f1b03dd3824ee11cd6bcb31d6ef8caefd0a619
samuelho-dev <samuelho343@gmail.com>
2023-05-16 21:14:49 -0700

    outline

---
+++ src/components/profile/payments.tsx
@@ -0,0 +1,13 @@
+import { GetServerSideProps } from "next";
+import React from "react";
+import { api } from "~/utils/api";
+
+function Payments() {
+  return (
+    <div>
+      <h2>Payments</h2>
+    </div>
+  );
+}
+
+export default Payments;
