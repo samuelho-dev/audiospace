00e1fe4c52df4f95723571b0e82eb653b673f15e
samuelho-dev <samuelho343@gmail.com>
2023-05-30 19:00:17 -0700

    rate limit sliding window

--- src/server/redis/rateLimit.ts
+++ src/server/redis/rateLimit.ts
@@ -3,7 +3,7 @@ import { Redis } from "@upstash/redis";
 
 export const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.tokenBucket(5, "10 s", 10),
+  limiter: Ratelimit.slidingWindow(5, "10 s"),
   analytics: true,
   prefix: "@upstash/ratelimit",
 });
