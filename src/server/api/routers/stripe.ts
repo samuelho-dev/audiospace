53f906045ba2a82cce1daa6c5ebc7f9f474e4101
samuelho-dev <samuelho343@gmail.com>
2023-06-06 00:14:07 -0700

    changed query to mutation to fetch account url

--- src/server/api/routers/stripe.ts
+++ src/server/api/routers/stripe.ts
@@ -1,5 +1,6 @@
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
+import { env } from "~/env.mjs";
 
 import {
   createTRPCRouter,
@@ -8,7 +9,7 @@ import {
 } from "~/server/api/trpc";
 
 export const stripeRouter = createTRPCRouter({
-  sellerAccountCreate: protectedProcedure.query(async ({ ctx, input }) => {
+  sellerAccountCreate: protectedProcedure.mutation(async ({ ctx, input }) => {
     const account = await ctx.stripe.accounts.create({
       type: "standard",
       email: ctx.session.user.email,
@@ -23,9 +24,11 @@ export const stripeRouter = createTRPCRouter({
 
     const accountLink = await ctx.stripe.accountLinks.create({
       account: account.id,
-      refresh_url: "",
-      return_url: "",
+      refresh_url: `${env.NEXTAUTH_URL}/profile?section=seller-signup`,
+      return_url: `${env.NEXTAUTH_URL}/seller/onboaring/finish`,
       type: "account_onboarding",
     });
+
+    return accountLink.url;
   }),
 });
